2d5d25baa39385f3a8c5b2a427d7b62ca54a0ba2
Roman Shcherbinin <shcherbinin_rv@mail.ru>
2022-04-02 20:32:29 +0300

    test uncommented

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -1,34 +1,34 @@
-// const { calculateTotal } = require('../calculate');
+const { calculateTotal } = require('../calculate');
 
-// test('basic test', () => {
-//   expect(4).toBe(4);
-// });
+test('basic test', () => {
+  expect(4).toBe(4);
+});
 
-// test('calculateTotal sum', () => {
-//   const list = [
-//     {
-//       id: 456,
-//       name: 'Война и мир',
-//       count: 3,
-//       price: 400,
-//     },
-//     {
-//       id: 77,
-//       name: 'JavaScript',
-//       count: 1,
-//       price: 1300,
-//     },
-//   ];
-//   const result = calculateTotal(list);
-//   expect(result).toBe(2500);
-// });
+test('calculateTotal sum', () => {
+  const list = [
+    {
+      id: 456,
+      name: 'Война и мир',
+      count: 3,
+      price: 400,
+    },
+    {
+      id: 77,
+      name: 'JavaScript',
+      count: 1,
+      price: 1300,
+    },
+  ];
+  const result = calculateTotal(list);
+  expect(result).toBe(2500);
+});
 
-const t = function () {
-  return function (a, b) {
-    return a + b;
-  };
-};
+// const t = function () {
+//   return function (a, b) {
+//     return a + b;
+//   };
+// };
 
 // const te = t();
 
-console.log(t()(2, 8));
+// console.log(t()(2, 8));
