8c4b4231bc799d57f70a13d414655f0b9e5cea43
Roman Shcherbinin <shcherbinin_rv@mail.ru>
2022-04-02 16:18:48 +0300

    initial commit

---
+++ src/calculate.js
@@ -0,0 +1,13 @@
+function calculateTotal(purchases) {
+  let result = 0;
+  for (let i = 0; i < purchases.length; i += 1) {
+    result += purchases[i].count * purchases[i].price;
+  }
+  return result;
+}
+
+module.exports = {
+  calculateTotal,
+};
+
+console.log(test.each([[1, 2], [3, 4]]));
